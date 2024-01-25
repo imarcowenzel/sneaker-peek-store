6287a29781e16f3a62efc69716468d1f2174eb47
imarcowenzel <marcowenzelcirilo@gmail.com>
2024-01-25 15:04:08 -0300

    Not Found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,7 @@
 const NotFound = () => {
   return (
     <div className="mx-8 flex items-center justify-center md:mx-24 md:h-[100dvh]">
-      <div className="px-auto my-8 flex w-full flex-col items-center justify-center gap-y-4 bg-white py-20 text-center">
+      <div className="px-2 my-8 flex w-full flex-col items-center justify-center gap-y-4 bg-white py-20 text-center">
         <h1 className="text-2xl font-bold text-cyan-500 md:text-3xl lg:text-5xl">
           This page doesn't seem to exist.
         </h1>
