c138b5c03ea5ce2ebfdf8eef01f6548a769ef580
imarcowenzel <marcowenzelcirilo@gmail.com>
2024-01-25 14:55:55 -0300

    Not Found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,11 @@
 const NotFound = () => {
   return (
     <div className="mx-8 flex items-center justify-center md:mx-24 md:h-[100dvh]">
-      <div className="px-auto my-8 flex w-full flex-col items-center justify-center gap-y-4 whitespace-nowrap bg-white py-20 text-center">
-        <h1 className="text-xl font-bold text-cyan-500 md:text-3xl lg:text-5xl">
+      <div className="px-auto my-8 flex w-full flex-col items-center justify-center gap-y-4 bg-white py-20 text-center">
+        <h1 className="text-2xl font-bold text-cyan-500 md:text-3xl lg:text-5xl">
           This page doesn't seem to exist.
         </h1>
-        <h3 className="text-xs font-bold text-gray-500 md:text-2xl">
+        <h3 className="text-xs font-bold text-gray-500 md:text-xl">
           It looks like the link pointing here was faulty.
         </h3>
       </div>
