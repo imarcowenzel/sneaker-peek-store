784966aeb734ba5ffd9e105eb2e536135cca8712
imarcowenzel <marcowenzelcirilo@gmail.com>
2024-02-08 23:13:53 -0300

    Updating

--- src/app/(routes)/shop/page.tsx
+++ src/app/(routes)/shop/page.tsx
@@ -27,11 +27,13 @@ export const metadata: Metadata = {
 
 const Shop = () => {
   return (
-    <div className="mx-auto flex max-w-full justify-center bg-[#f2f4f6] px-5 text-center">
-      <div className="my-0 w-full py-5 xl:max-w-[1360px]">
+    <section className="mx-auto flex max-w-full justify-center bg-[#f2f4f6]  text-center">
+
+      <div className="my-0 w-full p-5 lg:p-8 lg:max-w-[1360px]">
+
         {/* Filter and Sorting Bar */}
         <div className="fixed bottom-0 left-0 right-0 z-50 w-full md:static ">
-          <div className="flex w-full items-center justify-between border-[1px] border-black border-opacity-10 bg-[#f8f8f8] px-5 py-3 md:border-none md:bg-transparent">
+          <div className="flex w-full items-center justify-between border-[1px] border-black border-opacity-10 bg-[#f8f8f8] px-5 py-3 md:p-0 md:border-none md:bg-transparent">
             <Sheet>
 
               <SheetTrigger className="flex hover:text-[#f2f4f6] rounded-full py-2 px-4 hover:bg-gray-800 items-center justify-between font-bold text-gray-800 gap-x-2 text-sm">
@@ -113,7 +115,8 @@ const Shop = () => {
           ))}
         </ul>
       </div>
-    </div>
+
+    </section>
   );
 };
 
