6107284a44913aae3051665f8bb59af89ab7766b
imarcowenzel <marcowenzelcirilo@gmail.com>
2024-03-06 11:47:38 -0300

    Updating cart sheet

--- src/components/ui/quantity-control.tsx
+++ src/components/ui/quantity-control.tsx
@@ -3,6 +3,8 @@
 import { MinusIcon, PlusIcon } from "lucide-react";
 
 import useCart, { Item } from "@/hooks/use-cart";
+import { DetailedHTMLProps, InputHTMLAttributes, useEffect, useRef, useState } from "react";
+import { Input } from "./input";
 
 interface QuantityControlprops {
   value: number;
@@ -10,35 +12,42 @@ interface QuantityControlprops {
 }
 
 const QuantityControl: React.FC<QuantityControlprops> = ({ value, item }) => {
+
   const cart = useCart();
-  console.log(item.quantity)
-  
 
   return (
     <div className="flex items-center">
-      <span
+      <div
         onClick={() =>
           cart.decreaseQuantity(item.product.id, item.selectedSize)
         }
         className="flex h-6 w-6 cursor-pointer items-center justify-center border text-lg md:h-9 md:w-9"
       >
         <MinusIcon className="h-3 w-3 md:h-4 md:w-4" />
-      </span>
+      </div>
+
+      <div className="flex h-6 w-6 items-center justify-center border text-center text-xs outline-none md:h-9 md:w-9 md:text-sm lg:text-base">
+        {value}
+      </div>
+{/* 
       <input
         type="number"
         pattern="^[0-9]+$"
         className="flex h-6 w-6 items-center justify-center border text-center text-xs outline-none md:h-9 md:w-9 md:text-sm lg:text-base"
-        value={item.quantity}
+        value={value}
         max={100}
-      />
-      <span
+        autoFocus={false}
+        ref={inputRef}
+      /> */}
+ 
+      <div
         onClick={() =>
           cart.increaseQuantity(item.product.id, item.selectedSize)
         }
         className="flex h-6 w-6 cursor-pointer items-center justify-center border text-lg md:h-9 md:w-9"
       >
         <PlusIcon className="h-3 w-3 md:h-4 md:w-4" />
-      </span>
+      </div>
     </div>
   );
 };
